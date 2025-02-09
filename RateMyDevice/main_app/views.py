fd5be215043df4270a52936560346b73e660f72c
nawraaman <nawra.aman55@gmail.com>
2025-02-09 18:33:39 +0300

    Home Done

---
+++ RateMyDevice/main_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Define the home view function
+def home(request):
+    return HttpResponse('<h1>Hello to Rate My Device Website</h1>')
+
+def about(request):
+    return render(request, 'about.html')
