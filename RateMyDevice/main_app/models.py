064fb22e19634413cda451cd9cc4b7ba802b65f7
Ima7moodz <imahmoodi@outlook.com>
2025-02-10 18:47:20 +0300

    model

--- RateMyDevice/main_app/models.py
+++ RateMyDevice/main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    rate = models.IntegerField(default=0)
+    warrenty_expration_Date =models.DateField()
+    opinion = models.TextField(max_length=250)
+    likes =models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
