650382b9f705ae2983e98362a533a011f522266c
Ima7moodz <imahmoodi@outlook.com>
2025-02-10 20:57:01 +0300

    10-2

--- RateMyDevice/main_app/urls.py
+++ RateMyDevice/main_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('', views.device_index, name='home'),
     path('about/', views.about, name='about'),
+
+    # CBV's for CREATE, UPDATE, and Delete
 ]
--- RateMyDevice/main_app/views.py
+++ RateMyDevice/main_app/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Device
+from django.views.generic import ListView, DetailView
+
 
 # Define the home view function
-def home(request):
-    return HttpResponse('<h1>Hello to Rate My Device Website</h1>')
+# def home(request):
+#     return HttpResponse('<h1>Hello to Rate My Device Website</h1>')
 
+def home(request):
+    return render(request, 'base.html')
 
 def about(request):
     return render(request, 'about.html')
+
+
+def device_index(request):
+    devices = Device.objects.all()
+
+    return render(request, 'base.html', {'devices': devices})
