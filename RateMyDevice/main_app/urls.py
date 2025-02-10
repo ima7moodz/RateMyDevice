de472a0181478aaacc87d5c26e6c9c5707fc0339
nawraaman <nawra.aman55@gmail.com>
2025-02-10 20:49:46 +0300

    device forms

--- RateMyDevice/main_app/urls.py
+++ RateMyDevice/main_app/urls.py
@@ -1,6 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from . import views 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+
+    path('device/create/', views.DeviceCreate.as_view(), name='device-create'),
 ]
