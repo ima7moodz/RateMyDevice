9b7bd0627f7675a8cd24ef66175a9a75ffeb86dc
Ima7moodz <imahmoodi@outlook.com>
2025-02-11 19:10:37 +0300

    device/index list adevices

--- RateMyDevice/main_app/urls.py
+++ RateMyDevice/main_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('', views.device_index, name='home'),
+    path('devices', views.device_index, name='device-ind'),
     path('about/', views.about, name='about'),
 
     # CBV's for CREATE, UPDATE, and Delete
--- RateMyDevice/main_app/views.py
+++ RateMyDevice/main_app/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Device
-<<<<<<< HEAD
-from django.views.generic import ListView, DetailView
-
-
-=======
 from django.views.generic import CreateView, UpdateView,DeleteView
 from django.views.generic import ListView, DetailView
->>>>>>> e43404fecd832bc18483013fbf0557ff035d7e48
 # Define the home view function
 # def home(request):
 #     return HttpResponse('<h1>Hello to Rate My Device Website</h1>')
@@ -19,14 +13,13 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-<<<<<<< HEAD
 
 def device_index(request):
     devices = Device.objects.all()
 
-    return render(request, 'base.html', {'devices': devices})
-=======
+    return render(request, 'devices/index.html', {'devices': devices})
+
+
 class DeviceCreate(CreateView):
     model = Device
     fields = ['name', 'category', 'description', 'rate', 'warrenty_expration_Date', 'opinion']
->>>>>>> e43404fecd832bc18483013fbf0557ff035d7e48
