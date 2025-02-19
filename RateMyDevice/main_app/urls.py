f8fef29bdd7f65183185700a526b5208803d3415
Ima7moodz <imahmoodi@outlook.com>
2025-02-19 16:34:42 +0300

    finish style and fixing some chat error

--- RateMyDevice/main_app/urls.py
+++ RateMyDevice/main_app/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     # Chat
-    path('chat/start/<int:user_id>/', views.start_chat, name='start-chat'),
     path('chat/<int:chat_id>/', views.chat_room, name='chat-room'),
+    path('chat/start/<int:user_id>/', views.start_chat, name='start-chat'),
 
     # Profile
     path('profile/<str:username>/', views.profile_view, name='profile'),
